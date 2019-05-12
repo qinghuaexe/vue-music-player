48fe0dee3b23a0a492d5c0f740b6e57854655d7d
qinghuaexe <qinghuaexe@gmail.com>
2019-05-12 14:33:16 +0800

    项目代码重构

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    排行榜
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    推荐歌曲
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus"></style>
