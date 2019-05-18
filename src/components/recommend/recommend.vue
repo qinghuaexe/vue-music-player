7a1f8499ff110173b9c3862051b9fcf060195dda
qinghuaexe <qinghuaexe@gmail.com>
2019-05-18 15:43:23 +0800

    成功抓取轮播数据

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -12,10 +12,25 @@
   </div>
 </template>
 <script>
-export default {}
+import { getRecommend } from '../../api/recommend'
+import { ERR_OK } from '../../api/config'
+export default {
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
+}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '../../common/stylus/variable';
+@import '../../common/stylus/variable'
 
 .recommend {
   position: fixed;
