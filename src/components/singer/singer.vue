13564642e8e461576c17fe404d85b5b9b5188c7c
qinghuaexe <qinghuaexe@gmail.com>
2019-05-20 21:25:35 +0800

    抓取数据成功

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -4,6 +4,27 @@
   </div>
 </template>
 <script>
-export default {}
+import { getSingerList } from '../../api/singer'
+import { ERR_OK } from '../../api/config'
+export default {
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(res.data.list)
+        }
+      })
+    }
+  }
+}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus"></style>
