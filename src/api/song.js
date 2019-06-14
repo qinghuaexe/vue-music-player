49224e0ca0011d18c0e7e83f5cf02947d0fcd5a1
qinghuaexe <qinghuaexe@gmail.com>
2019-06-14 22:33:09 +0800

    获取歌词

--- src/api/song.js
+++ src/api/song.js
@@ -1,4 +1,6 @@
 import jsonp from '../common/js/jsonp'
+import { commonParams } from './config'
+import axios from 'axios'
 
 export function getSongVkey(songmid) {
   const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
@@ -15,4 +17,22 @@ export function getSongVkey(songmid) {
     filename: `C400${songmid}.m4a`
   })
   return jsonp(url, data)
+}
+
+export function getLyric(mid) {
+  const url = '/lyric'
+  const data = Object.assign({}, commonParams, {
+    songmid: mid,
+    pcachetime: +new Date(),
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    g_tk: 67232076,
+    format: 'json'
+  })
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
