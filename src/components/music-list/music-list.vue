4fda7b3b7dd61b56ca1abebac4106ec4a075d149
qinghuaexe <qinghuaexe@gmail.com>
2019-06-14 18:24:13 +0800

    随机播放全部

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -6,7 +6,7 @@
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" v-show="songs.length > 0" ref="playBtn">
+        <div class="play" v-show="songs.length > 0" ref="playBtn" @click="random">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -84,7 +84,12 @@ export default {
     back() {
       this.$router.back()
     },
-    ...mapActions(['selectPlay'])
+    random() {
+      this.randomPlay({
+        list: this.songs
+      })
+    },
+    ...mapActions(['selectPlay', 'randomPlay'])
   },
   watch: {
     scrollY(newY) {
