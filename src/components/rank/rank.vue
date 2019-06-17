57cbdc4671e512a2000cb6ca911294f54530b950
qinghuaexe <qinghuaexe@gmail.com>
2019-06-17 21:45:39 +0800

    榜单详情数据应用

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -2,7 +2,7 @@
   <div class="rank" ref="rank">
     <scroll :data="topList" class="toplist" ref="toplist">
       <ul>
-        <li class="item" v-for="(item, index) in topList" :key="index">
+        <li class="item" v-for="(item, index) in topList" :key="index" @click="selectItem(item)">
           <div class="icon">
             <img width="100" height="100" v-lazy="item.picUrl">
           </div>
@@ -27,6 +27,7 @@ import { ERR_OK } from '../../api/config'
 import Scroll from '../../base/scroll/scroll'
 import Loading from '../../base/loading/loading'
 import { playlistMixin } from '../../common/js/mixin'
+import { mapMutations } from 'vuex'
 export default {
   mixins: [playlistMixin],
   data() {
@@ -47,6 +48,15 @@ export default {
       this.$refs.rank.style.bottom = bottom
       this.$refs.toplist.refresh()
     },
+    selectItem(item) {
+      this.$router.push({
+        path: `/rank/${item.id}`
+      })
+      this.setTopList(item)
+    },
+    ...mapMutations({
+      setTopList: 'SET_TOP_LIST'
+    }),
     _getTopList() {
       getTopList().then(res => {
         if (res.code === ERR_OK) {
---
+++ src/components/top-list/top-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <transition name="slider">
+    <music-list :title="title" :bg-image="bgImage"></music-list>
+  </transition>
+</template>
+<script>
+import MusicList from '../music-list/music-list'
+import { mapGetters } from 'vuex'
+export default {
+  components: {
+    MusicList
+  },
+  computed: {
+    title() {
+      console.log(this.topList)
+      return this.topList.topTitle
+    },
+    bgImage() {
+      return this.topList.picUrl
+    },
+    ...mapGetters([
+      'topList'
+    ])
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s;
+}
+
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
+</style>
