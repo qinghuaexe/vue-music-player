6dcc69c9f8db34c03127eefe1abf0b47097b6bfa
qinghuaexe <qinghuaexe@gmail.com>
2019-06-17 12:52:09 +0800

    推荐歌单详情与播放完成

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -43,6 +43,19 @@ export function createSong(musicData, songVkey) {
   })
 }
 
+export function createRecomSong (musicData, songVkey) {
+  return new Song({
+    id: musicData.id,
+    mid: musicData.mid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.name,
+    album: musicData.album.name,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.album.mid}.jpg?max_age=2592000`,
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.mid}.m4a?fromtag=38&guid=5931742855&vkey=${songVkey}`
+  })
+}
+
 function filterSinger(singer) {
   let ret = []
   if (!singer) {
--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 <script>
@@ -8,7 +8,14 @@ import MusicList from '../music-list/music-list'
 import { mapGetters } from 'vuex'
 import { getSongList } from '../../api/recommend'
 import { ERR_OK } from '../../api/config'
+import { createRecomSong } from '../../common/js/song'
+import { getSongVkey } from '../../api/song'
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
     title() {
       return this.disc.dissname
@@ -23,11 +30,28 @@ export default {
   },
   methods: {
     _getSongList() {
-      getSongList(this.disc.dissid).then((res) => {
+      if (!this.disc.dissid) {
+        this.$router.push('/recommend')
+      }
+      getSongList(this.disc.dissid).then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.cdlist[0].songlist)
+          this.songs = this._normalizeSongs(res.cdlist[0].songlist)
         }
       })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach(musicData => {
+        console.log(musicData)
+        console.log(musicData.mid)
+        getSongVkey(musicData.mid).then(res => {
+          const songVkey = res.data.items[0].vkey
+          if (musicData.id && musicData.album.id) {
+            ret.push(createRecomSong(musicData, songVkey))
+          }
+        })
+      })
+      return ret
     }
   },
 
