185a5df929bcd41ff7973220df3a54f1e94210a0
qinghuaexe <qinghuaexe@gmail.com>
2019-06-20 14:53:44 +0800

    添加playlist静态页面

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -82,6 +82,7 @@
         </div>
       </div>
     </transition>
+    <playlist></playlist>
     <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime" @ended="end"></audio>
   </div>
 </template>
@@ -95,6 +96,7 @@ import { shuffle } from '../../common/js/util'
 import Lyric from 'lyric-parser'
 import Scroll from '../../base/scroll/scroll'
 import { prefixStyle } from '../../common/js/dom'
+import Playlist from '../playlist/playlist'
 
 const transform = prefixStyle('transform')
 const transitionDuration = prefixStyle('transitionDuration')
@@ -114,7 +116,8 @@ export default {
   components: {
     ProgressBar,
     ProgressCircle,
-    Scroll
+    Scroll,
+    Playlist
   },
   computed: {
     playIcon() {
