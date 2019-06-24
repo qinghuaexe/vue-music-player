3d6872cefb6df14d70ac31f18e2e06406f5dcc66
qinghuaexe <qinghuaexe@gmail.com>
2019-06-24 14:35:15 +0800

    创建top-tip组件

---
+++ src/base/top-tip/top-tip.vue
@@ -0,0 +1,30 @@
+<template>
+  <transition name="drop">
+    <div class="top-tip">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+<script>
+export default {}
+</script>
+<style lang="stylus" scoped>
+@import '../../common/stylus/variable'
+
+.top-tip {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 500;
+  background: $color-dialog-background;
+
+  &.drop-enter-active, &.drop-leave-active {
+    transition: all 0.3s;
+  }
+
+  &.drop-enter, &.drop-leave-to {
+    transform: translate3d(0, -100%, 0);
+  }
+}
+</style>
+
