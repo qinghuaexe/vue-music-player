198fe2bd60baae5c727a906f1e7ee08803b8468d
qinghuaexe <qinghuaexe@gmail.com>
2019-06-26 09:04:44 +0800

    添加playlist收藏

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -14,8 +14,8 @@
             <li class="item" ref="listItem" v-for="(item, index) in sequenceList" :key="item.id" @click="selectItem(item, index)">
               <i class="current" :class="getCurrentIcon(item)"></i>
               <span class="text">{{item.name}}</span>
-              <span class="like">
-                <i class="icon-not-favorite"></i>
+              <span class="like" @click="toggleFavorite(item)">
+                <i :class="getFavoriteIcon(item)"></i>
               </span>
               <span class="delete" @click.stop="deleteOne(item)">
                 <i class="icon-delete"></i>
