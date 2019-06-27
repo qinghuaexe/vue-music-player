678ab45d6ad5ae6aa8e85c613415de67afb134ad
qinghuaexe <qinghuaexe@gmail.com>
2019-06-27 21:06:13 +0800

    删除注释代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Recommend from '../components/recommend/recommend.vue'
-// import Rank from '../components/rank/rank.vue'
-// import Singer from '../components/singer/singer.vue'
-// import Search from '../components/search/search.vue'
-// import SingerDetail from '../components/singer-detail/singer-detail.vue'
-// import Disc from '../components/disc/disc.vue'
-// import TopList from '../components/top-list/top-list.vue'
-// import UserCenter from '../components/user-center/user-center.vue'
 
 Vue.use(Router)
 
